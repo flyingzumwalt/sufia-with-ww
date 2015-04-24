a648a50f069607827bae2af6cedcf367233b665e
Matt Zumwalt <matt@curationexperts.com>
2015-04-24 11:30:28 -0500

    generated sufia aa app (sufia 6.0.0)

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '897f12b9e150300818ffe5ec5e222fa8712348fded906e1229b3fd519b14fac93cc71625b1a72406022036dcff6c7da10c54cc4e3f760bbe7bcecdcb136c0c75'
+
